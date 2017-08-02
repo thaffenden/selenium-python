8bb977800fac15776d73c2f6f4b77fcb2691d225
Tristan Haffenden <tristan.haffenden@qumu.com>
2017-08-02 08:59:36 +0100

    Fixes dict iteration in _make_w3c_caps function

--- selenium/webdriver/remote/webdriver.py
+++ selenium/webdriver/remote/webdriver.py
@@ -5,7 +5,7 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-#
+#:
 #   http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing,
@@ -67,7 +67,7 @@ def _make_w3c_caps(caps):
     """
     profile = caps.get('firefox_profile')
     always_match = {}
-    for k, v in caps.iteritems():
+    for k, v in caps.items():
         if k in _W3C_CAPABILITY_NAMES or ':' in k:
             always_match[k] = v
     if profile:
